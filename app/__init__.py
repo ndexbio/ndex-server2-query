a06a47725898e9a8ade403827555de16368fcc4d
Aaron Gary <agary@ucsd.edu>
2016-12-16 14:17:15 -0800

    Update endpoint for Dev SOLR

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,14 @@ import logging.handlers
 root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 log_path = os.path.join(root, 'logs')
 mongodb_uri = 'mongodb://localhost'
-solr_url = 'http://preview.ndexbio.org:8983/solr/'
-#solr_url = 'http://dev2.ndexbio.org:8983/solr/'
+#solr_url = 'http://preview.ndexbio.org:8983/solr/'
+solr_url = 'http://dev.ndexbio.org:8983/solr/'
 
-#repo_directory = '/Users/aarongary/Development/DataSets/NDEx/server2/data/'
-repo_directory = '/opt/ndex/data/'
+repo_directory = '/Users/aarongary/Development/DataSets/NDEx/server2/data/'
+#repo_directory = '/opt/ndex/data/'
 
-#temp_append_path = '/Users/aarongary/Development/Projects/ndex-python/ndex'
-temp_append_path = '/opt/ndex/query_engine/ndex-python/ndex'
+temp_append_path = '/Users/aarongary/Development/Projects/ndex-python/ndex'
+#temp_append_path = '/opt/ndex/query_engine/ndex-python/ndex'
 
 
 def get_logger(name, level=logging.DEBUG):
